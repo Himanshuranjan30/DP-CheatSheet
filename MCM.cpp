fdaf2be540b6557356adc7d35ead9e186500de1c
Himanshuranjan30 <himanshuranjan30@gmail.com>
2020-12-23 12:24:49 +0530

    Update MCM.cpp

--- MCM.cpp
+++ MCM.cpp
@@ -1,13 +1,45 @@
-int MCM(int arr[],int l,int r,int n)
-{
-	if(l<0||l>=n||r<0||r>=n)
-		return -1;
-	int ans=INT_MAX;
-	for(int i=l;i<r;i++)
-	{
-		int left=MCM(arr,l,i,n);
-		int right=MCM(arr,i+1,r,n);
-		ans=min(left,right);
-	}
-	return ans;
-}
+/* A naive recursive implementation that simply 
+follows the above optimal substructure property */
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Matrix Ai has dimension p[i-1] x p[i] 
+// for i = 1..n 
+int MatrixChainOrder(int p[], int i, int j) 
+{ 
+	if (i == j) 
+		return 0; 
+	int k; 
+	int min = INT_MAX; 
+	int count; 
+
+	// place parenthesis at different places 
+	// between first and last matrix, recursively 
+	// calculate count of multiplications for 
+	// each parenthesis placement and return the 
+	// minimum count 
+	for (k = i; k < j; k++) 
+	{ 
+		count = MatrixChainOrder(p, i, k) 
+				+ MatrixChainOrder(p, k + 1, j) 
+				+ p[i - 1] * p[k] * p[j]; 
+
+		if (count < min) 
+			min = count; 
+	} 
+
+	// Return minimum count 
+	return min; 
+} 
+
+// Driver Code 
+int main() 
+{ 
+	int arr[] = { 1, 2, 3, 4, 3 }; 
+	int n = sizeof(arr) / sizeof(arr[0]); 
+
+	cout << "Minimum number of multiplications is "
+		<< MatrixChainOrder(arr, 1, n - 1); 
+} 
+
+// This code is contributed by Shivi_Aggarwal
